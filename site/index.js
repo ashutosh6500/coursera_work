c7b81309d9e59bc1c96f1684cb7a65e9a3e651c9
Ashutosh <ashutoshdkodavade@gmail.com>
2022-01-12 15:39:54 +0530

    clock

--- site/index.js
+++ site/index.js
@@ -1,1 +1,14 @@
-document.getElementById("demo").innerHTML = "Welcome to Javascript programming";
+func =()=>{
+	let date=new Date();
+let timestring="";
+timestring+=date.getHours();
+timestring+=":";
+timestring+=date.getMinutes();
+timestring+=":";
+timestring+=date.getSeconds();
+document.getElementById("para").innerHTML=timestring;
+document.getElementById("date").innerHTML=date.toLocaleDateString();
+}
+//setInterval(func,1000); //This will give real time date and time.
+func();
+
